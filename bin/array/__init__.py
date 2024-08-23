64ee33326bfb462aa96f3cd1a0c2e22db376fc35
active0831 <leejaehak87@gmail.com>
2024-08-23 23:02:32 +0900

    0.0.15

--- bin/array/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2024 Jaehak Lee
-
-from . import array_dict_data
-from . import array_to_image
-from . import axis_rotation
-from . import labeledTensor
-from . import text_matrix_expression
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='turtletree',
-    version='0.0.11',
+    version='0.0.15',
     description='Data transform library written by Jaehak Lee',
     author='Jaehak Lee',
     author_email='leejaehak87@gmail.com',
@@ -14,7 +14,7 @@ setup(
     install_requires=['numpy', 'pandas', 'matplotlib',],
     packages=find_packages(exclude=[]),
     keywords=['data', 'database', 'array', 'matrix'],
-    python_requires='>=3.10',
+    python_requires='>=3.6',
     package_data={},
     zip_safe=False,
     classifiers=[
